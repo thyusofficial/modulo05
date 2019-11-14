742ba6c63f820a080635f0ebff3e86ae80dada51
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2019-11-14 20:30:11 -0300

    styles, api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,54 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
 
-export default function Main() {
-    return <Title>Hello World</Title>;
+import { Container, Form, SubmitButton } from './styles';
+
+export default class Main extends Component {
+    state = {
+        newRepo: '',
+        repositories: []
+    };
+
+    handleInpputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const { newRepo, repositories } = this.state;
+        const response = await api.get(`/repos/${newRepo}`);
+        const data = {
+            name: response.data.full_name
+        };
+        this.setState({
+            repositories: [...repositories, data],
+            newRepo: ''
+        });
+    };
+
+    render() {
+        const { newRepo } = this.state;
+        return (
+            <Container>
+                <h1>
+                    <FaGithubAlt />
+                    Repositórios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repositório"
+                        value={newRepo}
+                        onChange={this.handleInpputChange}
+                    />
+
+                    <SubmitButton>
+                        <FaPlus color="#FFF" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }
