d429b3ea357e64b1828fbb27a86b961d16acc5b2
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2019-11-19 14:11:28 -0300

    desafio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
-
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
     state = {
         newRepo: '',
-        repositories: []
+        repositories: [],
+        loading: false,
+        error: null
     };
+
+    componentDidMount() {
+        const repositories = localStorage.getItem('repositories');
+
+        if (repositories) {
+            this.setState({ repositories: JSON.parse(repositories) });
+        }
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { repositories } = this.state;
+        if (prevState.repositories !== repositories) {
+            localStorage.setItem('repositories', JSON.stringify(repositories));
+        }
+    }
 
     handleInpputChange = e => {
         this.setState({ newRepo: e.target.value });
@@ -16,19 +34,31 @@ export default class Main extends Component {
 
     handleSubmit = async e => {
         e.preventDefault();
-        const { newRepo, repositories } = this.state;
-        const response = await api.get(`/repos/${newRepo}`);
-        const data = {
-            name: response.data.full_name
-        };
-        this.setState({
-            repositories: [...repositories, data],
-            newRepo: ''
-        });
+        this.setState({ loading: true });
+
+        try {
+            const { newRepo, repositories } = this.state;
+
+            const repoExists = repositories.find(r => r.name === newRepo);
+
+            if (repoExists) throw new Error('Repositório duplicado');
+
+            const response = await api.get(`/repos/${newRepo}`);
+            const data = {
+                name: response.data.full_name
+            };
+            this.setState({
+                repositories: [...repositories, data],
+                newRepo: '',
+                loading: false
+            });
+        } catch (error) {
+            this.setState({ error: true });
+        }
     };
 
     render() {
-        const { newRepo } = this.state;
+        const { newRepo, repositories, loading, error } = this.state;
         return (
             <Container>
                 <h1>
@@ -36,7 +66,7 @@ export default class Main extends Component {
                     Repositórios
                 </h1>
 
-                <Form onSubmit={this.handleSubmit}>
+                <Form onSubmit={this.handleSubmit} error={error}>
                     <input
                         type="text"
                         placeholder="Adicionar repositório"
@@ -44,10 +74,29 @@ export default class Main extends Component {
                         onChange={this.handleInpputChange}
                     />
 
-                    <SubmitButton>
-                        <FaPlus color="#FFF" size={14} />
+                    <SubmitButton loading={loading}>
+                        {loading ? (
+                            <FaSpinner color="#FFF" size={14} />
+                        ) : (
+                            <FaPlus color="#FFF" size={14} />
+                        )}
                     </SubmitButton>
                 </Form>
+
+                <List>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
+                            <Link
+                                to={`/repository/${encodeURIComponent(
+                                    repository.name
+                                )}`}
+                            >
+                                Detalhes
+                            </Link>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
     }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,24 +1,4 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    max-width: 700px;
-    background: #fff;
-    border-radius: 4px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-    padding: 30px;
-    margin: 80px auto;
-
-    h1 {
-        font-size: 20px;
-        display: flex;
-        align-items: center;
-        flex-direction: row;
-    }
-
-    svg {
-        margin-right: 10px;
-    }
-`;
+import styled, { keyframes, css } from 'styled-components';
 
 export const Form = styled.form`
     margin-top: 30px;
@@ -27,14 +7,25 @@ export const Form = styled.form`
 
     input {
         flex: 1;
-        border: 1px solid #eee;
+        border: 2px solid ${props => (props.error ? '#ff6b6b' : '#eee')};
         padding: 10px 15px;
         border-radius: 4px;
         font-size: 16px;
     }
 `;
 
-export const SubmitButton = styled.button.attrs({ type: 'submit' })`
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }to{
+        transform: rotate(360deg);
+    }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.loading
+}))`
     background: #7159c1;
     border: 0;
     padding: 0 15px;
@@ -43,4 +34,38 @@ export const SubmitButton = styled.button.attrs({ type: 'submit' })`
     display: flex;
     align-items: center;
     justify-content: center;
+
+    &[disabled] {
+        cursor: not-allowed;
+        opacity: 0.6;
+    }
+    ${props =>
+        props.loading &&
+        css`
+            svg {
+                animation: ${rotate} 2s linear infinite;
+            }
+        `}
+`;
+
+export const List = styled.ul`
+    list-style: none;
+    margin-top: 30px;
+
+    li {
+        padding: 15px 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        & + li {
+            border-top: 1px solid #eee;
+        }
+
+        a {
+            color: #7159c1;
+            text-decoration: none;
+        }
+    }
 `;
